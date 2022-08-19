6c24ea4bf7d8ad00c1145e0281349791aeeb11a7
naveens3590 <naveens3590@gmail.com>
2022-08-19 11:50:51 -0400

    Americold project

---
+++ src/main/java/com/aws/s3/service/CommonService.java
@@ -0,0 +1,34 @@
+package com.aws.s3.service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+public class CommonService {
+	/**
+	 * This method first deletes all the files in given folder and than the folder
+	 * itself
+	 */
+	public static void createFolder(String bucketName, String folderName, AmazonS3 client,String SUFFIX) {
+		// create meta-data for your folder and set content-length to 0
+		ObjectMetadata metadata = new ObjectMetadata();
+		metadata.setContentLength(20);
+		 String data = "Hello Americold Member";
+
+		// create empty content+
+		InputStream content = new ByteArrayInputStream(data.getBytes());
+		//InputStream content = IOUtils.toInputStream("Hello Americold", StandardCharsets.UTF_8);
+		// create a PutObjectRequest passing the folder name suffixed by /
+		PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, folderName + SUFFIX, content,
+				metadata);
+
+		// send request to S3 to create folder
+		client.putObject(putObjectRequest);
+	}
+
+}
+	
+
